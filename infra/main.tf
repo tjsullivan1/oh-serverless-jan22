44e463b46d326c1c510dceceec7d0972aabc3666
Tim Sullivan (CSA) <tisulliv@microsoft.com>
2022-01-03 13:17:32 -0600

    moved infra to main folder

---
+++ infra/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_version = "~> 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "registry.terraform.io/hashicorp/azurerm"
+      version = "> 2.72.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = false
+  features {}
+}
+
+data "azurerm_subscription" "primary" {}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-tjs-oh-challenge2"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "stafa" {
+  name                     = "stafatjs"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "asp-tjs-challenge2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "challenge2" {
+  name                       = "challenge2"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.registry.name
+  app_service_plan_id        = azurerm_app_service_plan.asp.id
+  storage_account_name       = azurerm_storage_account.stafa.name
+  storage_account_access_key = azurerm_storage_account.stafa.primary_access_key
+  os_type                    = "linux"
+  version                    = "~3"
+}
