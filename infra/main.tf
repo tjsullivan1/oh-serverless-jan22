4b1cf4b63437653a2489a41f6822a0cedcef8778
Tim Sullivan (CSA) <tisulliv@microsoft.com>
2022-01-03 14:07:21 -0600

    ubuntu-latest has terraform 1.1.2, updated code to match

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.0.0"
+  required_version = "~> 1.1.0"
   required_providers {
     azurerm = {
       source  = "registry.terraform.io/hashicorp/azurerm"
@@ -53,7 +53,7 @@ resource "azurerm_app_service_plan" "asp" {
 resource "azurerm_function_app" "challenge2" {
   name                       = "challenge2"
   location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
+  resource_group_name        = azurerm_resource_group.rg.namegh
   app_service_plan_id        = azurerm_app_service_plan.asp.id
   storage_account_name       = azurerm_storage_account.stafa.name
   storage_account_access_key = azurerm_storage_account.stafa.primary_access_key
